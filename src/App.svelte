2501cc46d19f6b1dbf9e22840008f2a9f33bce19
PaulLagadec <paul.lagadec@oclock.school>
2025-01-14 11:18:40 +0100

    ieta-setup-sucessfull

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+    // Importer le router
+    import Router from 'svelte-spa-router'
+
+    // Importer les composants
+    import Header from './lib/components/Header.svelte'
+    import Footer from './lib/components/Footer.svelte'
+
+    // Importer nos pages
+    import Home from "./lib/pages/Home.svelte";
+    import NotFound from "./lib/pages/NotFound.svelte";
+
+    const routes = {
+        "/": Home,
+        "*": NotFound,
+    }
+</script>
+
+<Header />
+<Router {routes} />
+<Footer />
---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,13 @@
+<script>
+    import {link} from "svelte-spa-router"
+</script>
+
+<header>
+    <nav>
+        <ul>
+            <li>
+                <a href="/" use:link aria-label="Retour à l'accueil">Accueil</a>
+            </li>
+        </ul>
+    </nav>
+</header>
